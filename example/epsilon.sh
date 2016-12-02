f88965180eac8b8ad67ced5790dcb5141d2eba82
Mario Prausa <prausa@physik.rwth-aachen.de>
2016-12-02 15:49:26 +0100

    example added

---
+++ example/epsilon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p out
+
+epsilon-prepare matrix.m > out/matrix
+
+epsilon --timings --symbols r3 --load out/matrix 1 1 --queue out/queue --fermat enable.r3.fer \
+	--fuchsify --normalize --factorep-at -1 \
+	--block 2 3 --fuchsify --normalize --factorep-at -1 --left-fuchsify \
+	--block 4 4 --fuchsify --normalize --factorep-at -1 --left-fuchsify \
+	--block 5 9 --fuchsify --normalize --factorep-at  1 --left-fuchsify \
+	--block 1 9 --factorep-at -1 \
+	--write out/epsilon --export out/transformation.m
+exit $?
+
