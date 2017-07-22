mprausa/epsilon

commit bdcd55ca7422d9f8b1a8bcc1601e047d7272af85

get rid of regex

epsilon/src/TransformationQueue.cpp
@@ -23,7 +23,6 @@
 #include <sstream>
 #include <iostream>
 #include <algorithm>
-#include <regex>
 #include <System.h>
 using namespace std;
 
@@ -71,7 +70,6 @@ string TransformationQueue::filename() {
 
 void TransformationQueue::load(string _filename) {
     ifstream file(_filename);
-    cmatch what;
 	string str;
     
     Fermat *fermat = infinity.fer();
@@ -95,18 +93,23 @@ void TransformationQueue::load(string _filename) {
         string str0(str,0,colon);
         str = str.erase(0,colon+1);
 
-        if (regex_match(str0.c_str(),what,regex("^B\\(([^,]+),([^,]+)\\)$"))) {
+        if (str0.substr(0,2) == "B[" && str0.back() == ']') {
+            size_t comma = str0.find(',');
+            if (comma == string::npos) {
+                throw invalid_argument("parse error");
+            }
+            
             trans.type = transformation_t::Balance;
             string xstr;
 
-            xstr = string(what[1].first,what[1].second);
+            xstr = str0.substr(2,comma-2);
             if (xstr == "inf") {
                 trans.x1 = infinity;
             } else {
                 trans.x1 = FermatExpression(fermat,xstr);
             }
-            
-            xstr = string(what[2].first,what[2].second);
+
+            xstr = str0.substr(comma+1,str0.size()-comma-2);
             if (xstr == "inf") {
                 trans.x2 = infinity;
             } else {
@@ -122,19 +125,22 @@ void TransformationQueue::load(string _filename) {
             trans.k = 0;
 
             trans.T = FermatArray(fermat,str);
-        } else if (regex_match(str0.c_str(),what,regex("^L\\(([^,]+),([^,]+)\\)$"))) {
-            trans.type = transformation_t::LeftTrans;
-            string xstr;
+        } else if (str0.substr(0,2) == "L[" && str0.back() == ']') {
+            size_t comma = str0.find(',');
+            if (comma == string::npos) {
+                throw invalid_argument("parse error");
+            }
 
-            xstr = string(what[1].first,what[1].second);
+            trans.type = transformation_t::LeftTrans;
+            string xstr(str0,2,comma-2);
+
             if (xstr == "inf") {
                 trans.x1 = infinity;
             } else {
                 trans.x1 = FermatExpression(fermat,xstr);
             }
             
-            xstr = string(what[2].first,what[2].second);
-            trans.k = atoi(xstr.c_str());
+            trans.k = stoi(str0.substr(comma+1,str0.size()-comma-2));
 
             trans.x2 = zero;
 

epsilon/src/System.cpp
@@ -27,10 +27,10 @@
 #include <Echelon.h>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <iomanip>
 #include <stdexcept>
 #include <algorithm>
-#include <regex>
 using namespace std;
 
 FermatExpression infinity;
@@ -75,7 +75,6 @@ System::System(Fermat *fermat, bool echfer) : tqueue(fermat) {
 
 System::System(Fermat *fermat, string filename, int start, int end, bool echfer) : tqueue(fermat) {
 	string str;
-    cmatch what;
 	ifstream file(filename);
     int r;
     
@@ -99,14 +98,19 @@ System::System(Fermat *fermat, string filename, int start, int end, bool echfer)
 
         string str0(str,0,colon);
         str = str.erase(0,colon+1);
-        
-        if (regex_match(str0.c_str(),what,regex("^A\\[([^,]+),(\\d+)\\]$"))) {
+       
+        if (str0.substr(0,2) == "A[" && str0.back() == ']') {
+            size_t comma = str0.find(',');
+            if (comma == string::npos) {
+                throw invalid_argument("parse error");
+            }
+
             sing_t singularity;
             TriangleBlockMatrix mat;
 
-            singularity.point = FermatExpression(fermat,string(what[1].first,what[1].second));
-            singularity.rank = atoi(string(what[2].first,what[2].second).c_str());
-          
+            singularity.point = FermatExpression(fermat,str0.substr(2,comma-2));
+            singularity.rank = stoi(str0.substr(comma+1,str0.size()-comma-2));
+
             FermatArray array(fermat,str);
             r = array.rows();
             if (end<0) end=r;
@@ -141,12 +145,10 @@ System::System(Fermat *fermat, string filename, int start, int end, bool echfer)
                     singularities[singularity.point].rank = singularity.rank;
                 }
             }
-        } else if (regex_match(str0.c_str(),what,regex("^B\\[(\\d+)\\]$"))) {
-            int k;
+        } else if (str0.substr(0,2) == "B[" && str0.back() == ']') {
+            int k = stoi(str0.substr(2,str0.size()-3));
             TriangleBlockMatrix mat;
             int r;
-
-            k = atoi(string(what[1].first,what[1].second).c_str());
             
             FermatArray array(fermat,str);
             r = array.rows();

commit e448f99c94c2264ca07cf845e328d88c548153ac

fix bug introduced in bdcd55ca7422d9f8b1a8bcc1601e047d7272af85

epsilon/src/TransformationQueue.cpp
@@ -93,7 +93,7 @@ void TransformationQueue::load(string _filename) {
         string str0(str,0,colon);
         str = str.erase(0,colon+1);
 
-        if (str0.substr(0,2) == "B[" && str0.back() == ']') {
+        if (str0.substr(0,2) == "B(" && str0.back() == ')') {
             size_t comma = str0.find(',');
             if (comma == string::npos) {
                 throw invalid_argument("parse error");
@@ -125,7 +125,7 @@ void TransformationQueue::load(string _filename) {
             trans.k = 0;
 
             trans.T = FermatArray(fermat,str);
-        } else if (str0.substr(0,2) == "L[" && str0.back() == ']') {
+        } else if (str0.substr(0,2) == "L(" && str0.back() == ')') {
             size_t comma = str0.find(',');
             if (comma == string::npos) {
                 throw invalid_argument("parse error");